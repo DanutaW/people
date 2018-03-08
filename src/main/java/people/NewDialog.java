89b13bda5dbae8d340fe691855bef17331a724d4
Danuta Witczak <datra.witczak@gmail.com>
2018-03-08 18:50:33 +0100

    Poprawa kodu

--- src/main/java/people/NewDialog.java
+++ src/main/java/people/NewDialog.java
@@ -7,18 +7,17 @@ import javax.swing.*;
 
 public class NewDialog extends JDialog {
 
-    private JTextField name;
-    private JTextField surname;
-    private JSpinner age;
-    private JRadioButton rb1;
-    private JRadioButton rb2;
+    private JTextField nameField;
+    private JTextField surnameField;
+    private JSpinner ageSpinner;
+    private JRadioButton maleRadio;
+    private JRadioButton femaleRadio;
+    private JCheckBox sportCheckBox, mathCheckBox, musicCheckBox, historyCheckBox;
     private JButton okButton;
     private JButton cancelButton;
-    private JCheckBox cb1, cb2, cb3, cb4;
     private NewDialogListener listener;
 
     public NewDialog(NewDialogListener listener) {
-
         setModal(true);
 
         this.listener = listener;
@@ -26,93 +25,78 @@ public class NewDialog extends JDialog {
         setLayout(new BorderLayout());
         JPanel panel = new JPanel();
         panel.add(new JLabel("Imię: "));
-        panel.add(name = new JTextField(10));
+        panel.add(nameField = new JTextField(10));
         panel.add(new JLabel("Nazwisko: "));
-        panel.add(surname = new JTextField(10));
+        panel.add(surnameField = new JTextField(10));
         panel.add(new JLabel("Wiek: "));
         SpinnerModel value = new SpinnerNumberModel(1, 1, 100, 1);
-        panel.add(age = new JSpinner(value));
+        panel.add(ageSpinner = new JSpinner(value));
 
-        rb1 = new JRadioButton("Mężczyzna");
-        rb2 = new JRadioButton("Kobieta");
+        maleRadio = new JRadioButton("Mężczyzna");
+        femaleRadio = new JRadioButton("Kobieta");
         ButtonGroup bg = new ButtonGroup();
-        bg.add(rb1);
-        bg.add(rb2);
+        bg.add(maleRadio);
+        bg.add(femaleRadio);
 
-        panel.add(rb1);
-        panel.add(rb2);
+        panel.add(maleRadio);
+        panel.add(femaleRadio);
 
-        add(panel, BorderLayout.WEST);
+        panel.add( new JLabel("Zainteresowania: "));
+        panel.add(sportCheckBox = new JCheckBox("Sport"));
+        panel.add(mathCheckBox = new JCheckBox("Matematyka"));
+        panel.add(musicCheckBox = new JCheckBox("Muzyka"));
+        panel.add(historyCheckBox = new JCheckBox("Historia"));
 
-        JPanel panel2 = new JPanel();
-        JLabel lab;
-        panel2.add(lab = new JLabel("Zainteresowania: "));
-        panel2.add(cb1 = new JCheckBox("Sport"));
-        panel2.add(cb2 = new JCheckBox("Matematyka"));
-        panel2.add(cb3 = new JCheckBox("Muzyka"));
-        panel2.add(cb4 = new JCheckBox("Historia"));
-
-        add(panel2, BorderLayout.EAST);
+        add(panel, BorderLayout.CENTER);
 
         okButton = new JButton("Ok");
-        okButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-
-                String n = name.getText();
-                String s = surname.getText();
-                int a = (Integer) age.getValue();
-
-                String p = null;
-                if (rb1.isSelected()) {
-                    p = "Mężczyzna";
-                }
-                if (rb2.isSelected()) {
-                    p = "Kobieta";
-                }
-
-                ArrayList<String> listCheckBox = new ArrayList<>();
-                if (cb1.isSelected()) {
-                    listCheckBox.add(cb1.getText());
-                }
-                if (cb2.isSelected()) {
-                    listCheckBox.add(cb2.getText());
-                }
-                if (cb3.isSelected()) {
-                    listCheckBox.add(cb3.getText());
-                }
-                if (cb4.isSelected()) {
-                    listCheckBox.add(cb4.getText());
-                }
-
-                String str = String.join(",", listCheckBox);
-
-                ArrayList<String> errors = new ArrayList<>();
-
-                if (name.getText().isEmpty()) {
-                    errors.add("Imię musi zostać podane");
-                }
-                if (surname.getText().isEmpty()) {
-                    errors.add("Nazwisko musi zostać podane");
-                }
-                if (!rb1.isSelected() && !rb2.isSelected()) {
-                    errors.add("Płeć musi zostać wybrana");
-                }
-
-                if (!errors.isEmpty()) {
-                    String st = String.join("\n", errors);
-                    JOptionPane.showMessageDialog(null, st, "Error", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    listener.okClicked(n, s, a, p, str);
-                    setVisible(false);
-                }
+        okButton.addActionListener((ActionEvent event) -> {
+            String name1 = nameField.getText();
+            String surname = surnameField.getText();
+            int age = (Integer) ageSpinner.getValue();
+            String sex = null;
+            if (maleRadio.isSelected()) {
+                sex = "Mężczyzna";
+            }
+            if (femaleRadio.isSelected()) {
+                sex = "Kobieta";
+            }
+            ArrayList<String> listCheckBox = new ArrayList<>();
+            if (sportCheckBox.isSelected()) {
+                listCheckBox.add(sportCheckBox.getText());
+            }
+            if (mathCheckBox.isSelected()) {
+                listCheckBox.add(mathCheckBox.getText());
+            }
+            if (musicCheckBox.isSelected()) {
+                listCheckBox.add(musicCheckBox.getText());
+            }
+            if (historyCheckBox.isSelected()) {
+                listCheckBox.add(historyCheckBox.getText());
+            }
+            String hobbies = String.join(",", listCheckBox);
+            ArrayList<String> errors = new ArrayList<>();
+            if (nameField.getText().isEmpty()) {
+                errors.add("Imię musi zostać podane");
+            }
+            if (surnameField.getText().isEmpty()) {
+                errors.add("Nazwisko musi zostać podane");
+            }
+            if (!maleRadio.isSelected() && !femaleRadio.isSelected()) {
+                errors.add("Płeć musi zostać wybrana");
+            }
+            if (!errors.isEmpty()) {
+                String str = String.join("\n", errors);
+                JOptionPane.showMessageDialog(null, str, "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                listener.okClicked(name1, surname, age, sex, hobbies);
+                setVisible(false);
             }
         });
 
         cancelButton = new JButton("Anuluj");
-        cancelButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                setVisible(false);
-            }
+        cancelButton.addActionListener((ActionEvent event) -> {
+            setVisible(false);
         });
 
         JPanel buttonPanel = new JPanel();
@@ -121,6 +105,5 @@ public class NewDialog extends JDialog {
         add(buttonPanel, BorderLayout.SOUTH);
 
         pack();
-
     }
 }
